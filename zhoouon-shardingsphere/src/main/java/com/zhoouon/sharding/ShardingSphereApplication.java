6bee5843a4e4cb3ad5ab1482a6cb123f0a34d0cc
zhoudong <zhoouon@126.com>
2025-11-26 13:36:48 +0800

    删除监控模块

--- zhoouon-shardingsphere/src/main/java/com/zhoouon/sharding/ShardingSphereApplication.java
+++ zhoouon-shardingsphere/src/main/java/com/zhoouon/sharding/ShardingSphereApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * @Author: zhoudong
@@ -14,6 +15,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
  **/
 @SpringBootApplication
 @MapperScan(basePackages = {"com.zhoouon.sharding.mapper"})
+@EnableAspectJAutoProxy(exposeProxy = true)
 @RefreshScope
 @EnableDiscoveryClient
 public class ShardingSphereApplication {
