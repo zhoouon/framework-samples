0ed6e1f11d83f630b32dcdcc35e7287b3a74576b
zhoudong <zhoouon@126.com>
2026-09-07 21:32:16 +0800

    feat: deepseek ai代码测试

--- zhoouon-ai/src/test/java/com/zhoouon/ai/TestDeepseek.java
+++ zhoouon-ai/src/test/java/com/zhoouon/ai/TestDeepseek.java
@@ -1,8 +1,13 @@
 package com.zhoouon.ai;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.deepseek.DeepSeekChatModel;
+import org.springframework.ai.deepseek.DeepSeekChatOptions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 
 /**
  * @Author: zhoudong
@@ -13,9 +18,38 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(classes = {AiApplication.class})
 public class TestDeepseek {
 
+    /**
+     * 同步阻塞的方式进行输出
+     * @param chatModel
+     */
     @Test
-    public void testDeepseek(DeepSeekChatModel chatModel) {
+    public void testDeepseek1(@Autowired DeepSeekChatModel chatModel) {
         String call = chatModel.call("你好，你是谁");
         System.out.println(call);
+    }
+
+    /**
+     * 通过流的方式输出
+     * @param chatModel
+     */
+    @Test
+    public void testDeepseek2(@Autowired DeepSeekChatModel chatModel) {
+        Flux<String> stream = chatModel.stream("你好，你是谁");
+        stream.toIterable().forEach(System.out::println);
+    }
+
+    /**
+     * 通过温度的方式对输出内容进行人性化设置
+     * @param chatModel
+     */
+    @Test
+    public void testDeepseek3(@Autowired DeepSeekChatModel chatModel) {
+        DeepSeekChatOptions opetion = DeepSeekChatOptions.builder()
+                .temperature(0.1)
+                .model("deepseek-chat")
+                .build();
+        Prompt prompt = new Prompt("请写一句诗描述清晨", opetion);
+        ChatResponse response = chatModel.call(prompt);
+        System.out.println(response.getResult().getOutput().getText());
     }
 }
